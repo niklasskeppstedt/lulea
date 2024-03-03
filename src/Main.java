465dd5b5ed539880d14c2e4bde2f419428be3e82
niklasskeppstedt <niklas.skeppstedt@bontouch.com>
2024-03-03 17:28:19 +0100

    fix conflict

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    private static void printHelloWorld() {
         System.out.println("Hello sweden");
     }
 }
